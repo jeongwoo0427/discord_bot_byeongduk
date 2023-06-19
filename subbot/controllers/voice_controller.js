468e53efa175a74fb914f2119bb022e9db66f1c0
gg5111 <gg5111@naver.com>
2023-06-19 22:26:36 +0900

    fix: 병순이 VoiceContorller 수정

--- subbot/controllers/voice_controller.js
+++ subbot/controllers/voice_controller.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 //const discordTTS = require('discord-tts');
 const config = require('../config.json');
+const request = require('request');
 const { createAudioPlayer, createAudioResource, joinVoiceChannel, VoiceConnection } = require('@discordjs/voice');
 const splatSchedule = require('../module/splat3_schedule_module');
 const timeModule = require('../module/time_module');
@@ -13,10 +14,21 @@ initTempDir();
 const connections = new Map();
 
 
+const nameOfGuildId = {
+    '베이즈' : '1016607441707880468',
+    '겸사' : '942969471184805908',
+}
+
+
 const voiceController = {
     useUserTTS: async (message) => {
         try {
+            //console.log(connections);
 
+            if (message.member == null) {
+                message.reply('해당 기능은 서버에서만 사용할 수 있습니다.');
+                return;
+            }
 
             if (!message.member.voice.channel) {
                 message.reply('TTS를 사용하기 위해 먼저 음성채널에 있어야 합니다.')
@@ -31,15 +43,14 @@ const voiceController = {
 
             const guildId = message.guildId.toString();
             const channelId = message.member.voice.channel.id.toString();
-            
-            let author = message.author.id;
+
             let rawMessage = message.content;
-            let voice = getVoice(rawMessage);
-            let speed = getSpeed(rawMessage)
             let clearMessage = getClearMessage(rawMessage);
+            let voice = getVoice(rawMessage);
+            let speed = getSpeed(rawMessage);
 
 
-            const connectionState = connections[guildId]?.channels[channelId]?.state?.status;
+            const connectionState = connections[guildId]?.connection?.state?.status;
 
             console.log(`===============================================================================`);
             console.log(`${new Date().toString()}`);
@@ -51,92 +62,40 @@ const voiceController = {
 
                 if (connectionState == 'ready') {
                     //console.log('exit');
-                    
-                    connections[guildId].channels[channelId].destroy();
-                    connections[guildId].channels[channelId] = null;
+
+                    connections[guildId].connection.destroy();
+                    connections[guildId] = null;
                 }
                 return
             }
 
+            //접속상태, connections 객체의 channelId가 다르거나, 실제 접속된 커넥션의 channelId가 다를 경우
+            if (connectionState != `ready` || connections[guildId].channelId != channelId || connections[guildId]?.connection?.joinConfig?.channelId != channelId) {
 
-            if (connectionState != `ready`) {
+                connections[guildId] = { 
+                    connection : null, 
+                    channelId : null,
+                };
 
-                
+                console.log(`created new channel map`);
 
-                    connections[guildId] = {channels : new Map()};
-                    
-                    console.log(`created new channel map`);
-
-                    connections[guildId].channels[channelId] = joinVoiceChannel({
+                connections[guildId].connection = joinVoiceChannel({
                     guildId: guildId,
                     channelId: channelId,
                     adapterCreator: message.guild.voiceAdapterCreator
                 });
-                const connectionState2 = connections[guildId]?.channels[channelId]?.state?.status;
+                connections[guildId].channelId = channelId;
+                
+
+                const connectionState2 = connections[guildId]?.connection?.state?.status;
                 console.log(`${new Date().toString()} : joined voice channel`);
                 console.log(`connectionState2=${connectionState2}`);
                 // console.log(connection.);
             }
 
 
-            ////////////////////////메시지 변형부분////////////////////////
-            if (clearMessage == '스플' || clearMessage == '스플스케줄' || clearMessage == '스플스케쥴') {
-                const schedule = await splatSchedule.getSimpleSchdule();
-                speed = 1.0;
-                clearMessage = `현재 스플 스케줄을 알려드리겠습니다<break time="500ms"/>. 챌린지는 ${schedule.challenge}<break time="500ms"/>, 오픈은 ${schedule.open}<break time="500ms"/>, 연어는 ${schedule.salmon}이고 무기는 ${schedule.salmonWeapon[0]},${schedule.salmonWeapon[1]},${schedule.salmonWeapon[2]},${schedule.salmonWeapon[3]} 입니다, 오버플로셔,사랑해`
-            }
 
-            if (clearMessage == '시간') {
-                const text = timeModule.getTextTime();
-                speed = 1.0;
-                message.channel.send(text);
-                clearMessage = text;
-            }
-
-            //특정 인원 바보기능
-            //    if(author == '733572758499229766'){ //산들
-            //        clearMessage=`나는 바보다`;
-            //    }
-            ////////////////////////메시지 변형부분 끝////////////////////////
-
-
-            const xmlData =
-                `
-            <speak>
-            <voice name="`+ voice + `">
-            <prosody rate="`+ speed + `" volume="loud">
-            `
-                //+author+'님의 말  <break time="300ms"/>' 
-                + clearMessage
-                +
-                `
-            </prosody>
-            </voice>
-            </speak>
-            `;
-
-
-            await proRequest.download(
-                {
-
-                    method: "POST"
-                    , uri: "https://kakaoi-newtone-openapi.kakao.com/v1/synthesize"
-                    , headers: {
-                        "User-Agent": "Mozilla/5.0", 
-                        'Content-Type': 'application/xml',
-                        Authorization: `KakaoAK ${config.kakao_token}`,
-                    }, 
-                    encoding: null,
-                    body: xmlData,
-                },
-
-            `./temp_sub/tts/${guildId}.mp3`);
-            
-            let audioPlayer = createAudioPlayer();
-            const audioResource = createAudioResource(`./temp_sub/tts/${guildId}.mp3`); //사용을 위해서는 assets/audio/temp/tts 폴더가 존재해야 함.
-            audioPlayer.play(audioResource);
-            connections[guildId].channels[channelId].subscribe(audioPlayer);
-
+            await playVoice(clearMessage, voice, speed, guildId);
 
 
             // const post = request.post('https://kakaoi-newtone-openapi.kakao.com/v1/synthesize', {
@@ -152,38 +111,103 @@ const voiceController = {
             //     connections[guildId].channels[channelId].subscribe(audioPlayer);
             // });
             // post.pipe(fs.createWriteStream(`./temp/tts/${guildId}.mp3`),)
-         
+
 
         } catch (err) {
             message.channel.send('음성 모듈 관련 오류가 발생했습니다 ㅜㅜ');
         }
-    }
+    },
+    
 }
 
+
+async function playVoice(clearMessage, voice, speed, guildId) {
+    ////////////////////////메시지 변형부분////////////////////////
+
+
+    if (clearMessage == '스플' || clearMessage == '스플스케줄' || clearMessage == '스플스케쥴') {
+        const schedule = await splatSchedule.getSimpleSchdule();
+        speed = 1.0;
+        clearMessage = `현재 스플 스케줄을 알려드리겠습니다<break time="500ms"/>. 챌린지는 ${schedule.challenge}<break time="500ms"/>, 오픈은 ${schedule.open}<break time="500ms"/>, 연어는 ${schedule.salmon}이고 무기는 ${schedule.salmonWeapon[0]},${schedule.salmonWeapon[1]},${schedule.salmonWeapon[2]},${schedule.salmonWeapon[3]} 입니다, 오버플로셔,사랑해`
+    }
+
+    if (clearMessage == '시간') {
+        const text = timeModule.getTextTime();
+        speed = 1.0;
+        //message.channel.send(text);
+        clearMessage = text;
+    }
+
+    //특정 인원 바보기능
+    //    if(author == '733572758499229766'){ //산들
+    //        clearMessage=`나는 바보다`;
+    //    }
+    ////////////////////////메시지 변형부분 끝////////////////////////
+
+
+    const xmlData =
+        `
+                <speak>
+                <voice name="`+ voice + `">
+                <prosody rate="`+ speed + `" volume="loud">
+                `
+        //+author+'님의 말  <break time="300ms"/>' 
+        + clearMessage
+        +
+        `
+                </prosody>
+                </voice>
+                </speak>
+                `;
+
+
+    await proRequest.download(
+        {
+
+            method: "POST"
+            , uri: "https://kakaoi-newtone-openapi.kakao.com/v1/synthesize"
+            , headers: {
+                "User-Agent": "Mozilla/5.0",
+                'Content-Type': 'application/xml',
+                Authorization: `KakaoAK ${config.kakao_token}`,
+            },
+            encoding: null,
+            body: xmlData,
+        },
+
+        `./temp/sub_tts/${guildId}.mp3`);
+
+    let audioPlayer = createAudioPlayer();
+    const audioResource = createAudioResource(`./temp/sub_tts/${guildId}.mp3`); //사용을 위해서는 assets/audio/temp/tts 폴더가 존재해야 함.
+    audioPlayer.play(audioResource);
+    connections[guildId].connection.subscribe(audioPlayer);
+}
 
 function getVoice(message) {
 
-    const cmd1 = message.substr(0, 1);
-    const cmd2 = message.substr(1, 1);
+    // const cmd1 = message.substr(0, 1);
+    // const cmd2 = message.substr(1, 1);
 
-    //console.log(cmd1+cmd2);
+    // //console.log(cmd1+cmd2);
 
-    if (cmd1 == '^' && cmd2 != '^') return 'MAN_DIALOG_BRIGHT';
-    if (cmd1 == '^' && cmd2 == '^') return 'WOMAN_DIALOG_BRIGHT';
+    // if (cmd1 == '!' && cmd2 != '!') return 'MAN_DIALOG_BRIGHT';
+    // if (cmd1 == '*' && cmd2 != '*') return 'WOMAN_DIALOG_BRIGHT';
+    // if (cmd1 == '!' && cmd2 == '!') return 'MAN_READ_CALM';
+    // if (cmd1 == '*' && cmd2 == '*') return 'WOMAN_READ_CALM';
 
 
 
-    return 'MAN_DIALOG_BRIGHT';
+    return 'WOMAN_DIALOG_BRIGHT';
 }
 
-function getClearMessage(message){
+function getClearMessage(message) {
     const cmd1 = message.substr(0, 1);
     const cmd2 = message.substr(1, 1);
 
     let clearMessage = '';
 
-    if(cmd1 == '^') clearMessage = message.substring(1);
-    if(cmd2 == '^')clearMessage = message.substring(2);
+    if (cmd1 == '^' || cmd1 == '^') clearMessage = message.substring(1);
+    if (cmd2 == '^' || cmd2 == '^') clearMessage = message.substring(2);
 
     return clearMessage;
 }
@@ -197,14 +221,27 @@ function getSpeed(message) {
     return speed;
 }
 
+function getGuildId(message) {
+    if(!message.includes('[') || !message.includes(']')) return null;
+    let startIndex = message.indexOf("[") + 1;
+    let endIndex = message.indexOf("]");
+    let guildId = message.substring(startIndex, endIndex);
 
-function initTempDir(){
-    if(fs.existsSync(`./temp_sub`)){
-        fs.rmdirSync(`./temp_sub`,{recursive:true,force:true});
+    if(nameOfGuildId[guildId] != null){ //대체텍스트가 존재할경우 변환
+        return nameOfGuildId[guildId];
     }
 
-    fs.mkdirSync(`./temp_sub`);
-    fs.mkdirSync(`./temp_sub/tts`);
+    return guildId;
+}
+
+
+function initTempDir() {
+    if (fs.existsSync('./temp')) {
+        fs.rmdirSync('./temp', { recursive: true, force: true });
+    }
+
+    fs.mkdirSync('./temp');
+    fs.mkdirSync('./temp/sub_tts');
 }
 
 module.exports = voiceController;
